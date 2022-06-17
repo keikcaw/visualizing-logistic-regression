ab88d9c2b63c5710d2a170279764cc0741de8bc3
kaplanas <kaplanas@gmail.com>
2022-06-17 16:16:02 -0600

    add counterfactual visualization in the other direction

--- visual_folder/counterfactuals.R
+++ visual_folder/counterfactuals.R
@@ -71,3 +71,54 @@ with(
       theme_bw()
   }
 )
+
+# Plot the predicted number of "passes" among students who DIDN'T get tutoring,
+# compared to the number we think would have passed if they HAD gotten tutoring,
+# by group.  Account for uncertainty both in the parameter estimates and in the
+# outcomes for individual observations.
+with(
+  list(temp.df = expand.grid(draw = 1:1000,
+                             id = df$id[!df$tutoring]) %>%
+         left_join(df, by = "id") %>%
+         mutate(tutoring = T)),
+  {
+    temp.df %>%
+      mutate(mu = model.matrix(pass.m, data = temp.df) %*% coefs.df$est,
+             pred = runif(n()) < invlogit(mu)) %>%
+      group_by(pet.type, draw) %>%
+      summarise(n.passed = sum(pred),
+                .groups = "keep") %>%
+      ungroup() %>%
+      group_by(pet.type) %>%
+      summarise(lower.95 = quantile(n.passed, 0.025),
+                lower.50 = quantile(n.passed, 0.25),
+                upper.50 = quantile(n.passed, 0.75),
+                upper.95 = quantile(n.passed, 0.975),
+                n.passed = median(n.passed)) %>%
+      ungroup() %>%
+      mutate(pass.type = "Predicted") %>%
+      bind_rows(df %>%
+                  filter(!tutoring) %>%
+                  group_by(pet.type) %>%
+                  summarise(n.passed = sum(passed)) %>%
+                  ungroup() %>%
+                  mutate(pass.type = "Actual")) %>%
+      mutate(pet.type = str_to_title(pet.type),
+             pet.type = fct_reorder(pet.type, n.passed, max)) %>%
+      ggplot(aes(x = pet.type, color = pass.type, shape = pass.type)) +
+      geom_linerange(aes(ymin = lower.95, ymax = upper.95), size = 1) +
+      geom_linerange(aes(ymin = lower.50, ymax = upper.50), size = 2) +
+      geom_point(aes(y = n.passed), size = 3) +
+      scale_color_manual(values = c("brown3", "black")) +
+      scale_shape_manual(values = c(18, 16)) +
+      guides(color = guide_legend(override.aes = list(linetype = 0)),
+             shape = guide_legend(override.aes = list(linetype = 0))) +
+      labs(x = "Pet type",
+           y = "Number of untutored students predicted to pass with tutoring",
+           color = "", shape = "",
+           title = "Estimated number of untutored students who would have passed with tutoring",
+           subtitle = "By type of pet") +
+      coord_flip() +
+      theme_bw()
+  }
+)
