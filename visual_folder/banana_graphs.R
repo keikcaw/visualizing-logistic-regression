58139cf245efefd61b5691bf129f1fc9f8c8047a
Keiko Cawley <kcawley2@AAB355KCawley2.MSRM>
2022-07-27 20:51:59 -0600

    created a skeleton of the talk in Intro.Rmd, spruced up the introduction again, and  I added ## ---- to the all the R files in the visual folder

--- visual_folder/banana_graphs.R
+++ visual_folder/banana_graphs.R
@@ -1,4 +1,9 @@
-# ---------------------------- Banana Graph ------------------------------------
+library(cowplot)
+library(grid)
+library(gridExtra)
+library(patchwork)
+
+## ---- create-banana-graph ----
 # log odds to probability function
 log.to.prob <- function(value) {
     probability <- (exp(value)/(1+exp(value)))
@@ -18,15 +23,15 @@ create.banana.graph <- function(df, coefficient) {
     # first get the coefficient of interest
     get.coefficient <- as.numeric(
         df %>%
-            filter(predictor == coefficient) %>%
-            dplyr::select(slope)
+            filter(parameter == coefficient) %>%
+            dplyr::select(est)
     )
 
     # get the standard error for the coefficient of interest
     get.std.error <- as.numeric(
         df %>%
-            filter(predictor == coefficient) %>%
-            dplyr::select(std.error)
+            filter(parameter == coefficient) %>%
+            dplyr::select(se)
     )
     # use those values to create a data frame that will be used by the graph
     result <- as.data.frame(1:99) %>%
@@ -76,53 +81,58 @@ create.banana.graph <- function(df, coefficient) {
 
 
 
-# ---- tutoring
-create.banana.graph(get.coef.df, tutoringTRUE) +
-    facet_wrap(~ 'Tutoring') +
-    xlab('Predicted probability of student passing without tutoring') +
-    ylab('Predicted probability of student passing with tutoring') +
-    labs(color = "Effect")
 
-# ---- pet type
-pet.fish <- create.banana.graph(get.coef.df, pet.typefish) +
-    facet_wrap(~ 'Fish')
-pet.dog <- create.banana.graph(get.coef.df, pet.typedog) +
-    facet_wrap(~ 'Dog')
-pet.cat <- create.banana.graph(get.coef.df, pet.typecat) +
-    facet_wrap(~ 'Cat')
-
-# --- mac/glasses
-create.banana.graph(get.coef.df, macTRUE) +
-    facet_wrap(~ 'Mac User') +
-    xlab('Predicted probability of student passing if they do not use a Mac') +
+## ---- banana-graph-bare ----
+# create a banana graph for students who own a pet fish
+create.banana.graph(coefs.df, pet.typefish) +
+    facet_wrap(~ 'Fish') +
+    xlab('Predicted probability of student passing if they do not own a pet fish') +
     ylab('Predicted probability of student passing if they do own a pet fish')
-create.banana.graph(get.coef.df, glassesTRUE) +
-    facet_wrap(~ 'Wear Glasses') +
-    xlab('Predicted probability of student passing if they do not wear glasses') +
-    ylab('Predicted probability of student passing if they do wear glasses')
 
-
-# --- colors
-create.banana.graph(get.coef.df, favorite.colorred)
-create.banana.graph(get.coef.df, favorite.colororange)
-create.banana.graph(get.coef.df, favorite.colorgreen)
-
-
-
-
-#-----------------------Banana Graph figure sketches ---------------------------
-###### To show that we can aid in the interpretation of the graph by annotating
-pet.fish +
+## ---- banana-graph-annotated ----
+create.banana.graph(coefs.df, pet.typefish) +
+    facet_wrap(~ 'Fish') +
     xlab('Predicted probability of student passing if they do not own a pet fish') +
     ylab('Predicted probability of student passing if they do own a pet fish') +
-    annotate("pointrange", x = 0.4, y = 0.615, ymin = 0.4, ymax =0.615,
+    annotate("pointrange", x = 0.4, y = 0.165, ymin = 0.17, ymax =0.4,
              color = "black", size = 0.5) +
-    annotate("text", x = 0.4, y = 0.34, size = 2.90, hjust = 0,
-             label ="Example point:\nSome student who does not own a pet fish has a 40% chance of passing (x-axis value).\nHowever, if that same student did own a pet fish then their predicted probability of passing\nwould be approximately 61% (y-axis value). For this particular student, owning a pet fish\nincreased their chances of passing by 21% (distance from the dashed line to the black point).")
+    annotate("text", x = 0.41, y = 0.05, size = 2.65, hjust = 0,
+             label ="Example point:\nSome student who does not own a pet fish has a 40% chance of passing (x-axis\nvalue). However, if that same student did own a pet fish then their predicted\nprobability of passing would be approximately 17% (y-axis value). For this particular\nstudent, owning a pet fish decreased their chances of passing by 23% (distance\nfrom the ndashed line to the black point).")
 
 
 
-####### To show that showing many of these can get overwhelming on the eyes
+
+
+## ---- banana-graph-multiple ----
+y.grob <- textGrob("Predicted probability of the student passing if they own the pet",
+                   gp = gpar(col = "black", font.size = 9), rot = 90)
+x.grob <- textGrob("Predicted probability of the student passing if they do not own the pet",
+                   gp = gpar(col = "black", font.size = 11))
+
+grid.arrange(
+    arrangeGrob(
+        plot_grid(
+            (wrap_plots(create.banana.graph(coefs.df, pet.typefish) +
+                            facet_wrap(~ 'Fish') + 
+                            theme(axis.title = element_blank()),
+                        create.banana.graph(coefs.df, pet.typedog) +
+                            facet_wrap(~ 'Dog') + 
+                            theme(axis.title = element_blank(),
+                                  legend.title = element_blank()),
+                        create.banana.graph(coefs.df, pet.typecat) +
+                            facet_wrap(~ 'Cat') + 
+                            theme(axis.title = element_blank(),
+                                  legend.title = element_blank()),
+                        nrow = 1) +
+                 plot_annotation(title = "Passing based on the pet a student owns")
+             & theme(title = element_text(face = 'bold')))
+        ),
+        left = y.grob, bottom = x.grob
+    )
+)
+
+
+## ---- banana-graph-less-polished ----
 plot_grid(
     pet.fish +
         facet_wrap(~ 'Fish') +
@@ -143,24 +153,3 @@ plot_grid(
     rel_widths = c(1, 0.25, 1, 0.25, 1)
 )
 
-####### Show that we can lighten the load by adding common axes
-y.grob <- textGrob("Predicted probability of the student passing if they own the pet",
-                   gp = gpar(col = "black", font.size = 9), rot = 90)
-x.grob <- textGrob("Predicted probability of the student passing if they do not own the pet",
-                   gp = gpar(col = "black", font.size = 11))
-
-grid.arrange(
-    arrangeGrob(
-        plot_grid(
-            (wrap_plots(pet.fish + theme(axis.title = element_blank()),
-                        pet.dog + theme(axis.title = element_blank(),
-                                        legend.title = element_blank()),
-                        pet.cat + theme(axis.title = element_blank(),
-                                        legend.title = element_blank()),
-                        nrow = 1) +
-                 plot_annotation(title = "Passing based on the pet a student owns")
-             & theme(title = element_text(face = 'bold')))
-        ),
-        left = y.grob, bottom = x.grob
-    )
-)
